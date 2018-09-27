a0bc5d2fc6f529f6085298dfe9dedf7b9a9da0ce
devedzic <devedzic@gmail.com>
2018-09-28 01:03:18 +0200

    Initial structure. First view (index). RedirectView applied to make 'music/' the landing page. static() used to enable serving of static files during development.

---
+++ music/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1>Because the night belongs to lovers...</h1>")
+
+
